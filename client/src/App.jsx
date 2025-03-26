b448941875079ca5b35d0fb4d92bcc2ae795f139
awakeneddev <awakeneddevs@gmail.com>
2025-03-26 19:36:45 +0545

    Initial screen

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,31 @@
-import { useContext, useEffect } from "react";
-import { ConnectionContext } from "./context/context";
+import { Ticket } from "lucide-react";
+import { PrimaryButton, SecondaryButton } from "./components/ui/button/button";
+import { LotteryCard } from "./components/ui/card/LotteryCard";
+import { Intro } from "./components/ui/Intro";
+import { LOTTERIES } from "./data";
 
 function App() {
-  const { isConnected, setIsConnected } = useContext(ConnectionContext);
-
-  console.log("connection : ",isConnected)
-  useEffect(() => {
-    setIsConnected(true)
-  },[])
-  console.log("connection : ",isConnected)
   return (
-    <div className="bg-red-400 h-screen ">
-      <p className="text-white">{import.meta.env.VITE_CONTRACT_ADDRESS}</p>
+    <div className="min-h-screen bg-background bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400/10 via-[#0f172a] to-[#0f172a] ">
+      <div className="container mx-auto py-12">
+        {/* Action Buttons */}
+        <div className="flex justify-between items-center mb-8">
+          <SecondaryButton
+            label="Create Lottery"
+            icon={<Ticket className="h-4 w-5 mr-2" />}
+          />
+          <PrimaryButton label="Buy Ticket" />{" "}
+        </div>
+
+        {/* Intro Section */}
+        <Intro />
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {LOTTERIES.map((lottery, i) => (
+            <LotteryCard key={i} lottery={lottery} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
