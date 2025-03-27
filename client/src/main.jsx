ca90f8b5858e0037030846eda2a69ca27f0e53f9
awakeneddev <awakeneddevs@gmail.com>
2025-03-27 11:21:20 +0545

    Wrapedd with router provider and toast

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import { ConnectionProvider } from "./context/context.jsx";
 import "./index.css";
+import router from "./routes/routes.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ConnectionProvider>
-      <App />
+      <RouterProvider router={router} />
+      <ToastContainer position="bottom-right" />
     </ConnectionProvider>
   </StrictMode>
 );
