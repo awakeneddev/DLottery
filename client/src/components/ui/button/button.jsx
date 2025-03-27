af4fd6b4774c7fa567fe54d6ff12fe5656ec2ea7
awakeneddev <awakeneddevs@gmail.com>
2025-03-27 21:47:08 +0545

    refined

--- client/src/components/ui/button/button.jsx
+++ client/src/components/ui/button/button.jsx
@@ -1,16 +1,25 @@
+import { Loader } from "lucide-react";
+
 const SecondaryButton = ({ label, icon }) => {
   return (
-    <button className="flex items-center px-4 py-2 rounded-lg font-medium transition-all duration-300 bg-sky-400/20 text-sky-400 border border-emerald-400/20">
+    <button className="flex items-center px-4 py-2 rounded-lg font-semibold transition-all duration-300 bg-sky-400/20 text-sky-400 border border-emerald-400/20">
       {icon}
       {label}
     </button>
   );
 };
 
-const PrimaryButton = ({ label }) => {
+const PrimaryButton = ({ label, isLoading = false, onClick }) => {
   return (
-    <button className="px-6 py-2 rounded-lg font-medium transition-all duration-300 bg-gradient-to-r from-sky-400 to-emerald-400 text-gray-900 hover:shadow-lg hover:shadow-sky-400/20">
-      {label}
+    <button
+      onClick={onClick}
+      className="px-6 py-2 cursor-pointer rounded-lg font-semibold transition-all duration-300 bg-gradient-to-r from-sky-400 hover:from-sky-300 to-emerald-400 text-gray-900 hover:shadow-lg hover:shadow-sky-400/20"
+    >
+      {isLoading ? (
+        <Loader className="w-4 h-4 inline-block mr-2 text-gray-800/50" />
+      ) : (
+        `${label}`
+      )}
     </button>
   );
 };
