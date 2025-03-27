11c3928821e59e4d7231624b90320c066c10d86b
awakeneddev <awakeneddevs@gmail.com>
2025-03-27 21:45:26 +0545

    Wrapped with contexts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,22 @@
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-import { ConnectionProvider } from "./context/context.jsx";
+import { BlockChainProvider } from "./context/blockchainContext.jsx";
+import { IsConnectedProvider } from "./context/isConnectedContext.jsx";
 import "./index.css";
 import router from "./routes/routes.jsx";
+import { BrowserProvider } from "./context/browserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ConnectionProvider>
-      <RouterProvider router={router} />
-      <ToastContainer position="bottom-right" />
-    </ConnectionProvider>
+    <IsConnectedProvider>
+      <BlockChainProvider>
+        <BrowserProvider>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" /></BrowserProvider>
+      </BlockChainProvider>
+    </IsConnectedProvider>
   </StrictMode>
 );
