7601551f4e029b4952f2fa33b2a5400d6e9a4740
awakeneddev <awakeneddevs@gmail.com>
2025-04-01 14:34:34 +0545

    Deploying with proxy

--- contracts/scripts/deployInitial.js
+++ contracts/scripts/deployInitial.js
@@ -1,12 +1,28 @@
 const { ethers, network, upgrades } = require("hardhat");
 
 async function main() {
-  const lotteryFactory = await ethers.getContractFactory("Lottery");
+  const lotteryFactory = await ethers.getContractFactory("NextLotteryV1");
   console.log("Deploying contract to network :", network.name);
-
+  console.log(process.env.SUBSCRIPTION_ID,process.env.KEY_HASH,process.env.VRF_COORDINATOR)
+  const subscription_id = process.env.SUBSCRIPTION_ID;
+  const keyHash = process.env.KEY_HASH;
+  const vrfCoordinator = process.env.VRF_COORDINATOR;
+  const callbackGasLimit = 1000000;
+  const requestConfirmations = 3;
+  const numWords = 1;
   const lotteryContract = await upgrades.deployProxy(
-    lotteryFactory,[],
-    { initializer: "initialize" }
+    lotteryFactory,
+    [
+      vrfCoordinator,
+      subscription_id,
+      keyHash,
+      callbackGasLimit,
+      requestConfirmations,
+      numWords,
+    ],
+    {
+      initializer: "initialize",
+    }
   );
 
   await lotteryContract.waitForDeployment();
